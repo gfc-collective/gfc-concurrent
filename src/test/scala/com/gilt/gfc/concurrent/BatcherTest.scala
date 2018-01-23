3d968ebbf959a6ab0b37d2a66309468a1212663d
Gregor Heine <gheine@gilt.com>
2018-01-23 18:17:53 +0000

    Slow down test assertions to cater for crazy slow travis ci

--- src/test/scala/com/gilt/gfc/concurrent/BatcherTest.scala
+++ src/test/scala/com/gilt/gfc/concurrent/BatcherTest.scala
@@ -9,10 +9,12 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
 import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Span}
 
 
 class BatcherTest
   extends FunSuite with Matchers with Eventually {
+  implicit val patience = PatienceConfig(scaled(Span(500, Millis)))
 
   test("batcher works single-threadedly") {
     for ( maxOutstanding <- (1 to 10) ;
@@ -36,7 +38,7 @@ class BatcherTest
     val (batcher, adder, counter) = mkTestBatcher(100)
     records.foreach(i => batcher.add(i))
 
-    Thread.sleep(1001L) // should flush after 1sec
+    Thread.sleep(2001L) // should flush after 2sec
 
     eventually {
       adder.intValue shouldBe records.sum
@@ -64,7 +66,7 @@ class BatcherTest
     val records = (1 to 10000)
 
     val futures = records.map(i => Future{ batcher.add(i) } )
-    Await.result(Future.sequence(futures), 5 seconds) // should flush after 1sec
+    Await.result(Future.sequence(futures), 5 seconds) // should flush after 2sec
 
     adder.intValue shouldBe records.sum
 
@@ -74,23 +76,23 @@ class BatcherTest
 
   test("batcher adjusts next run to maxOutstandingDuration after reaching maxOutstandingCount") {
     val (batcher, adder, counter) = mkTestBatcher(2)
-    Thread.sleep(500L)
+    Thread.sleep(1000L)
 
-    // first add after 500ms should not flush immediately
+    // first add after 1sec, should not flush immediately
     batcher.add(1)
-    // second add should flush (still after ~500ms)
+    // second add should flush (still after ~1sec)
     batcher.add(1)
 
-    // should not flush again after 1sec schedule
-    Thread.sleep(600L)
+    // should not flush again after 2sec schedule
+    Thread.sleep(1100L)
     adder.intValue shouldBe 2
     counter.intValue shouldBe 1
 
     // third add should not flush immediately
     batcher.add(1)
 
-    // should flush on schedule after ~1.5sec
-    Thread.sleep(500L)
+    // should flush on schedule after ~3sec
+    Thread.sleep(1000L)
     eventually {
       adder.intValue shouldBe 3
     }
@@ -109,7 +111,7 @@ class BatcherTest
     val batcher = Batcher[Int](
       "test"
     , maxOutstandingCount
-    , 1 second
+    , 2 seconds
     ) { records =>
       callCounter.incrementAndGet()
       records.foreach(i => adder.addAndGet(i))
